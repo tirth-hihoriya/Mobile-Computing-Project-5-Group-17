9ff5ef2f1ed7f769dda896b0b8191cd5dcb7c918
karthikViswanathSriram <ksriram2@asu.edu>
2023-12-03 15:05:32 -0700

    Integrated Emotion detection with Main branch

--- app/src/main/java/com/example/guardianangel/MainActivity.java
+++ app/src/main/java/com/example/guardianangel/MainActivity.java
@@ -9,12 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button task1;
+    Button task1,task4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         task1 = findViewById(R.id.Task1);
+        task4=findViewById(R.id.Task4);
 
         task1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,10 +24,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        task4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task4();
+            }
+        });
+
     }
 
     private void Task1() {
         Intent intent1 = new Intent(this, Task1Main.class);
+        startActivity(intent1);
+    }
+
+    private void Task4() {
+        Intent intent1 = new Intent(this, EmotionAnalysis.class);
         startActivity(intent1);
     }
 }
